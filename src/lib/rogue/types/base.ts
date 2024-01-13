763567dddd025402eb81e3f0f60f8a562e89ff44
sigma <seiya.sugimoto.free@gmail.com>
2024-01-14 00:49:39 +0900

    add base type file

---
+++ src/lib/rogue/types/base.ts
@@ -0,0 +1,30 @@
+export type Coordinate = {
+  x: number,
+  y: number
+}
+
+export type Edge = {
+  from: Coordinate,
+  to: Coordinate,
+  edgeLength: number
+}
+
+export type Room = {
+  uuid: string,
+  center: Coordinate,
+  height: number,
+  width: number,
+  area: number,
+  topEdge: Edge,
+  bottomEdge: Edge,
+  rightEdge: Edge,
+  leftEdge: Edge
+}
+
+export type Corridor = {
+  uuid: string,
+  startRoomUuid: string,
+  endRoomUuid: string,
+  path: Coordinate[]
+}
+
