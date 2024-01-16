cd8e4c81cd7e703131fe07453d83e2b3fe527b91
sigma <seiya.sugimoto.free@gmail.com>
2024-01-16 23:19:10 +0900

    add roomToMatrixWithInnerSquare canvasMatrix

--- src/lib/rogue/canvasMatrix.ts
+++ src/lib/rogue/canvasMatrix.ts
@@ -1,7 +1,10 @@
 import type {
   Edge,
   GridCanvasMatrix,
-  RoomWithWall
+  Room,
+  RoomWithInnerSquare,
+  RoomWithWall,
+  Square
 } from "@/lib/rogue/types"
 
 export const initMatrix = (width: number, height: number) => {
@@ -30,41 +33,54 @@ export const edgeToMatrix = (matrix: GridCanvasMatrix, edge: Edge): GridCanvasMa
   return matrix
 }
 
-export const roomToMatrix = (matrix: GridCanvasMatrix, room: RoomWithWall, withThickness: boolean = false): GridCanvasMatrix => {
-  // roomのtopEdge, bottomEdge, leftEdge, rightEdgeを1にする
-  edgeToMatrix(matrix, room.outerSquare.top)
-  edgeToMatrix(matrix, room.outerSquare.bottom)
-  edgeToMatrix(matrix, room.outerSquare.right)
-  edgeToMatrix(matrix, room.outerSquare.left)
+export const squareToMatrix = (matrix: GridCanvasMatrix, square: Square): GridCanvasMatrix => {
+  edgeToMatrix(matrix, square.top)
+  edgeToMatrix(matrix, square.bottom)
+  edgeToMatrix(matrix, square.right)
+  edgeToMatrix(matrix, square.left)
+  return matrix
+}
 
-  if (withThickness) {
-    // 内側をthickness分だけ描画する
-    const { top: topEdge, bottom: bottomEdge, right: rightEdge, left: leftEdge } = room.outerSquare
-    const { top: topEdgeThickness, bottom: bottomEdgeThickness, right: rightEdgeThickness, left: leftEdgeThickness } = room.wall
-    for (let i = 1; i <= topEdgeThickness; i++) {
-      edgeToMatrix(matrix, {
-        from: { x: topEdge.from.x, y: topEdge.from.y + i },
-        to: { x: topEdge.to.x, y: topEdge.to.y + i }
-      })
-    }
-    for (let i = 1; i <= bottomEdgeThickness; i++) {
-      edgeToMatrix(matrix, {
-        from: { x: bottomEdge.from.x, y: bottomEdge.from.y - i },
-        to: { x: bottomEdge.to.x, y: bottomEdge.to.y - i }
-      })
-    }
-    for (let i = 1; i <= leftEdgeThickness; i++) {
-      edgeToMatrix(matrix, {
-        from: { x: leftEdge.from.x + i, y: leftEdge.from.y },
-        to: { x: leftEdge.to.x + i, y: leftEdge.to.y }
-      })
-    }
-    for (let i = 1; i <= rightEdgeThickness; i++) {
-      edgeToMatrix(matrix, {
-        from: { x: rightEdge.from.x - i, y: rightEdge.from.y },
-        to: { x: rightEdge.to.x - i, y: rightEdge.to.y }
-      })
-    }
+export const roomToMatrix = (matrix: GridCanvasMatrix, room: Room): GridCanvasMatrix => {
+  squareToMatrix(matrix, room.outerSquare)
+  return matrix
+}
+
+export const roomToMatrixWithWall = (matrix: GridCanvasMatrix, room: RoomWithWall): GridCanvasMatrix => {
+  squareToMatrix(matrix, room.outerSquare)
+
+  // 内側をthickness分だけ描画する
+  const { top: topEdge, bottom: bottomEdge, right: rightEdge, left: leftEdge } = room.outerSquare
+  const { top: topEdgeThickness, bottom: bottomEdgeThickness, right: rightEdgeThickness, left: leftEdgeThickness } = room.wall
+  for (let i = 1; i <= topEdgeThickness; i++) {
+    edgeToMatrix(matrix, {
+      from: { x: topEdge.from.x, y: topEdge.from.y + i },
+      to: { x: topEdge.to.x, y: topEdge.to.y + i }
+    })
   }
+  for (let i = 1; i <= bottomEdgeThickness; i++) {
+    edgeToMatrix(matrix, {
+      from: { x: bottomEdge.from.x, y: bottomEdge.from.y - i },
+      to: { x: bottomEdge.to.x, y: bottomEdge.to.y - i }
+    })
+  }
+  for (let i = 1; i <= leftEdgeThickness; i++) {
+    edgeToMatrix(matrix, {
+      from: { x: leftEdge.from.x + i, y: leftEdge.from.y },
+      to: { x: leftEdge.to.x + i, y: leftEdge.to.y }
+    })
+  }
+  for (let i = 1; i <= rightEdgeThickness; i++) {
+    edgeToMatrix(matrix, {
+      from: { x: rightEdge.from.x - i, y: rightEdge.from.y },
+      to: { x: rightEdge.to.x - i, y: rightEdge.to.y }
+    })
+  }
+  return matrix
+}
+
+export const roomToMatrixWithInnerSquare = (matrix: GridCanvasMatrix, room: RoomWithInnerSquare): GridCanvasMatrix => {
+  squareToMatrix(matrix, room.outerSquare)
+  squareToMatrix(matrix, room.innerSquare)
   return matrix
 }
