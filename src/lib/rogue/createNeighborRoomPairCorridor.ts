820cb42618873615d0b76d0a9e02d1bdf1a1ba4d
sigma <seiya.sugimoto.free@gmail.com>
2024-01-16 23:47:57 +0900

    fix corridor

--- src/lib/rogue/createNeighborRoomPairCorridor.ts
+++ src/lib/rogue/createNeighborRoomPairCorridor.ts
@@ -1,8 +1,8 @@
-import type { Coordinate, Corridor } from "@/lib/rogue/types/base"
+import type { Coordinate, Corridor, Room } from "@/lib/rogue/types/base"
 import type { NeighborRoomPair } from "@/lib/rogue/types/intermediate"
 import { v4 } from "uuid"
 
-export const neighborRoomPairToCorridor = (neighborRoomPair: NeighborRoomPair): Corridor => {
+export const neighborRoomPairToCorridor = <T extends Room>(neighborRoomPair: NeighborRoomPair<T>): Corridor => {
   const { room1, room2 } = neighborRoomPair
   const startRoomUuid = room1.uuid
   const endRoomUuid = room2.uuid
